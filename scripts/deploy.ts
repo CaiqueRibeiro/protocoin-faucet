a3b8660d27ee1678997c14c301783a443c71c299
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-02-24 20:45:00 -0300

    initial setup

---
+++ scripts/deploy.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+
+  const protoCoin = await ethers.deployContract("ProtoCoin");
+
+  await protoCoin.waitForDeployment();
+
+  const address = await protoCoin.getAddress();
+
+  console.log(`ProtoCoin deployed to: ${address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
